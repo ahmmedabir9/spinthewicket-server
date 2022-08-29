19a4c1997fef82c2fc74ee3a8d99c1442e0b4a59
Ahmmed Abir <ahmmedabir9@gmail.com>
2022-08-29 14:46:58 +0600

    two | six | four

--- src/functions/playMatch/runs/sixRuns.js
+++ src/functions/playMatch/runs/sixRuns.js
@@ -1,80 +1,68 @@
-const firebase = require("firebase-admin");
-const endOfOver = require("../end/endOfOver");
-const runChased = require("../end/runChased");
+const firebase = require('firebase-admin')
+const { UpdateQuickMatch } = require('../../../services/firebase')
+const endOfOver = require('../end/endOfOver')
+const runChased = require('../end/runChased')
 
-const sixRuns = async (matchData, docRef, inning) => {
-  const ball = {
-    wickets: matchData.now.wickets,
-    totalRuns: matchData.now.runs + 6,
-    run: 6,
-    bowler: matchData.now.bowler.name,
-    batsman: matchData.now.batsman.striker.name,
-    status: "six",
-    ballNO: matchData.now.balls + 1,
-    overNO: matchData.now.overs,
-  };
+const sixRuns = async (matchData, ballData, inning) => {
+  let dataToUpdate = {
+    'now.batsman.striker.runs': firebase.firestore.FieldValue.increment(6),
+    'now.batsman.striker.balls': firebase.firestore.FieldValue.increment(1),
+    'now.batsman.striker.sixes': firebase.firestore.FieldValue.increment(1),
+    'now.thisOver': firebase.firestore.FieldValue.arrayUnion({
+      ball: ball.ballNO,
+      status: ball.status,
+      run: ball.run,
+    }),
 
- await docRef
-    .update({
-      "now.batsman.striker.runs": firebase.firestore.FieldValue.increment(6),
-      "now.batsman.striker.balls": firebase.firestore.FieldValue.increment(1),
-      "now.batsman.striker.sixes": firebase.firestore.FieldValue.increment(1),
-      "now.thisOver": firebase.firestore.FieldValue.arrayUnion({
-        ball: ball.ballNO,
-        status: ball.status,
-        run: ball.run,
-      }),
+    'now.partnership': {
+      runs: matchData.now.partnership.runs + 6,
+      balls: matchData.now.partnership.balls + 1,
+      batsman1: matchData.now.batsman.striker.name,
+      batsman2: matchData.now.batsman.nonStriker.name,
+    },
+    'now.batsman.striker.strikeRate':
+      ((matchData.now.batsman.striker.runs + 6) /
+        (matchData.now.batsman.striker.balls + 1)) *
+      100,
+    'now.bowler.economy':
+      (matchData.now.bowler.runs + 6) /
+      ((matchData.now.bowler.overs * 6 + (matchData.now.bowler.balls + 1)) / 6),
+    [`innings.${inning}.ballByBall`]: firebase.firestore.FieldValue.arrayUnion(
+      ballData,
+    ),
+    'now.bowler.runs': firebase.firestore.FieldValue.increment(6),
+    'now.bowler.balls': firebase.firestore.FieldValue.increment(1),
+    'now.balls': firebase.firestore.FieldValue.increment(1),
+    'now.runs': firebase.firestore.FieldValue.increment(6),
+    'now.freeHit': false,
+    'now.runRate':
+      (matchData.now.runs + 6) /
+      ((matchData.now.overs * 6 + (matchData.now.balls + 1)) / 6),
+    [`innings.${inning}.runs`]: firebase.firestore.FieldValue.increment(6),
+    [`innings.${inning}.balls`]: firebase.firestore.FieldValue.increment(1),
+    [`innings.${inning}.runRate`]:
+      (matchData.now.runs + 6) /
+      ((matchData.now.overs * 6 + (matchData.now.balls + 1)) / 6),
+  }
 
-      "now.partnership": {
-        runs: matchData.now.partnership.runs + 6,
-        balls: matchData.now.partnership.balls + 1,
-        batsman1: matchData.now.batsman.striker.name,
-        batsman2: matchData.now.batsman.nonStriker.name,
-      },
-      "now.batsman.striker.strikeRate":
-        ((matchData.now.batsman.striker.runs + 6) /
-          (matchData.now.batsman.striker.balls + 1)) *
-        100,
-      "now.bowler.economy":
-        (matchData.now.bowler.runs + 6) /
-        ((matchData.now.bowler.overs * 6 + (matchData.now.bowler.balls + 1)) /
-          6),
-      [`innings.${inning}.ballByBall`]: firebase.firestore.FieldValue.arrayUnion(
-        ball
-      ),
-      "now.bowler.runs": firebase.firestore.FieldValue.increment(6),
-      "now.bowler.balls": firebase.firestore.FieldValue.increment(1),
-      "now.balls": firebase.firestore.FieldValue.increment(1),
-      "now.runs": firebase.firestore.FieldValue.increment(6),
-      "now.freeHit": false,
-      "now.runRate":
-        (matchData.now.runs + 6) /
-        ((matchData.now.overs * 6 + (matchData.now.balls + 1)) / 6),
-      [`innings.${inning}.runs`]: firebase.firestore.FieldValue.increment(6),
-      [`innings.${inning}.balls`]: firebase.firestore.FieldValue.increment(1),
-      [`innings.${inning}.runRate`]:
-        (matchData.now.runs + 6) /
-        ((matchData.now.overs * 6 + (matchData.now.balls + 1)) / 6),
-    })
-    .then(() => {
-      if (matchData.now.inning === 2 || matchData.now.inning === 4) {
-        docRef
-          .update({
-            "now.need": matchData.now.need - 6,
-            "now.from": matchData.now.from - 1,
-            "now.reqRR":
-              (matchData.now.need - 6) / ((matchData.now.from - 1) / 6),
-          })
-          .then(() => {
-            if (matchData.now.need <= 6) {
-              runChased(matchData, docRef, inning);
-            }
-          });
-      }
-      if (matchData.now.balls >= 5) {
-        endOfOver(matchData, docRef, inning);
-      }
-    });
-};
+  if (matchData.now.inning === 2 || matchData.now.inning === 4) {
+    dataToUpdate = {
+      ...dataToUpdate,
+      'now.need': matchData.now.need - 6,
+      'now.from': matchData.now.from - 1,
+      'now.reqRR': (matchData.now.need - 6) / ((matchData.now.from - 1) / 6),
+    }
+  }
 
-module.exports = sixRuns;
+  const updateMatch = await UpdateQuickMatch(matchData.id, dataToUpdate)
+
+  if (updateMatch.now.balls === 6) {
+    await endOfOver(updateMatch, inning)
+  }
+
+  if (updateMatch.now.need <= 0) {
+    await runChased(updateMatch, inning)
+  }
+}
+
+module.exports = sixRuns
--- src/functions/playMatch/runs/twoRuns.js
+++ src/functions/playMatch/runs/twoRuns.js
@@ -1,79 +1,66 @@
-const firebase = require("firebase-admin");
-const endOfOver = require("../end/endOfOver");
-const runChased = require("../end/runChased");
+const firebase = require('firebase-admin')
+const { UpdateQuickMatch } = require('../../../services/firebase')
+const endOfOver = require('../end/endOfOver')
+const runChased = require('../end/runChased')
 
-const twoRuns = async (matchData, docRef, inning) => {
-  const ball = {
-    wickets: matchData.now.wickets,
-    totalRuns: matchData.now.runs + 2,
-    run: 2,
-    bowler: matchData.now.bowler.name,
-    batsman: matchData.now.batsman.striker.name,
-    status: "run",
-    ballNO: matchData.now.balls + 1,
-    overNO: matchData.now.overs,
-  };
+const twoRuns = async (matchData, ballData, inning) => {
+  let dataToUpdate = {
+    'now.batsman.striker.runs': firebase.firestore.FieldValue.increment(2),
+    'now.batsman.striker.balls': firebase.firestore.FieldValue.increment(1),
+    'now.batsman.striker.strikeRate':
+      ((matchData.now.batsman.striker.runs + 2) /
+        (matchData.now.batsman.striker.balls + 1)) *
+      100,
+    [`innings.${inning}.ballByBall`]: firebase.firestore.FieldValue.arrayUnion(
+      ballData,
+    ),
+    'now.thisOver': firebase.firestore.FieldValue.arrayUnion({
+      ball: ball.ballNO,
+      status: ball.status,
+      run: ball.run,
+    }),
 
- await docRef
-    .update({
-      "now.batsman.striker.runs": firebase.firestore.FieldValue.increment(2),
-      "now.batsman.striker.balls": firebase.firestore.FieldValue.increment(1),
-      "now.batsman.striker.strikeRate":
-        ((matchData.now.batsman.striker.runs + 2) /
-          (matchData.now.batsman.striker.balls + 1)) *
-        100,
-      [`innings.${inning}.ballByBall`]: firebase.firestore.FieldValue.arrayUnion(
-        ball
-      ),
-      "now.thisOver": firebase.firestore.FieldValue.arrayUnion({
-        ball: ball.ballNO,
-        status: ball.status,
-        run: ball.run,
-      }),
+    'now.partnership': {
+      runs: matchData.now.partnership.runs + 2,
+      balls: matchData.now.partnership.balls + 1,
+      batsman1: matchData.now.batsman.striker.name,
+      batsman2: matchData.now.batsman.nonStriker.name,
+    },
+    'now.bowler.runs': firebase.firestore.FieldValue.increment(2),
+    'now.bowler.balls': firebase.firestore.FieldValue.increment(1),
+    'now.bowler.economy':
+      (matchData.now.bowler.runs + 2) /
+      ((matchData.now.bowler.overs * 6 + (matchData.now.bowler.balls + 1)) / 6),
+    'now.balls': firebase.firestore.FieldValue.increment(1),
+    'now.runs': firebase.firestore.FieldValue.increment(2),
+    'now.freeHit': false,
+    'now.runRate':
+      (matchData.now.runs + 2) /
+      ((matchData.now.overs * 6 + (matchData.now.balls + 1)) / 6),
+    [`innings.${inning}.runs`]: firebase.firestore.FieldValue.increment(2),
+    [`innings.${inning}.balls`]: firebase.firestore.FieldValue.increment(1),
+    [`innings.${inning}.runRate`]:
+      (matchData.now.runs + 2) /
+      ((matchData.now.overs * 6 + (matchData.now.balls + 1)) / 6),
+  }
 
-      "now.partnership": {
-        runs: matchData.now.partnership.runs + 2,
-        balls: matchData.now.partnership.balls + 1,
-        batsman1: matchData.now.batsman.striker.name,
-        batsman2: matchData.now.batsman.nonStriker.name,
-      },
-      "now.bowler.runs": firebase.firestore.FieldValue.increment(2),
-      "now.bowler.balls": firebase.firestore.FieldValue.increment(1),
-      "now.bowler.economy":
-        (matchData.now.bowler.runs + 2) /
-        ((matchData.now.bowler.overs * 6 + (matchData.now.bowler.balls + 1)) /
-          6),
-      "now.balls": firebase.firestore.FieldValue.increment(1),
-      "now.runs": firebase.firestore.FieldValue.increment(2),
-      "now.freeHit": false,
-      "now.runRate":
-        (matchData.now.runs + 2) /
-        ((matchData.now.overs * 6 + (matchData.now.balls + 1)) / 6),
-      [`innings.${inning}.runs`]: firebase.firestore.FieldValue.increment(2),
-      [`innings.${inning}.balls`]: firebase.firestore.FieldValue.increment(1),
-      [`innings.${inning}.runRate`]:
-        (matchData.now.runs + 2) /
-        ((matchData.now.overs * 6 + (matchData.now.balls + 1)) / 6),
-    })
-    .then(() => {
-      if (matchData.now.inning === 2 || matchData.now.inning === 4) {
-        docRef
-          .update({
-            "now.need": matchData.now.need - 2,
-            "now.from": matchData.now.from - 1,
-            "now.reqRR":
-              (matchData.now.need - 2) / ((matchData.now.from - 1) / 6),
-          })
-          .then(() => {
-            if (matchData.now.need <= 2) {
-              runChased(matchData, docRef, inning);
-            }
-          });
-      }
-      if (matchData.now.balls >= 5) {
-        endOfOver(matchData, docRef, inning);
-      }
-    });
-};
+  if (matchData.now.inning === 2 || matchData.now.inning === 4) {
+    dataToUpdate = {
+      'now.need': matchData.now.need - 2,
+      'now.from': matchData.now.from - 1,
+      'now.reqRR': (matchData.now.need - 2) / ((matchData.now.from - 1) / 6),
+    }
+  }
 
-module.exports = twoRuns;
+  const updateMatch = await UpdateQuickMatch(matchData.id, dataToUpdate)
+
+  if (updateMatch.now.balls === 6) {
+    await endOfOver(updateMatch, inning)
+  }
+
+  if (updateMatch.now.need <= 0) {
+    await runChased(updateMatch, inning)
+  }
+}
+
+module.exports = twoRuns
